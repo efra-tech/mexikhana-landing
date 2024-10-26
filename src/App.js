5d4d6bad89c8cf01e98139409d624bafbbe304d4
efra-tech <eahsan@uw.edu>
2024-10-26 14:04:15 -0700

    cloud

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import g from './Mexi Khana.png';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className=''>
+          <img src={g}></img>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
